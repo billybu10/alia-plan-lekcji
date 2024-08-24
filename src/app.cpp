c467a9f0f3fc01a70efa9978ac55a812af0e7a26
billybu10 <jakubb688@gmail.com>
2024-08-24 17:10:04 +0200

    Added checking corectness of user-provided values

--- src/app.cpp
+++ src/app.cpp
@@ -85,7 +85,18 @@ odliczanie(html::context ctx, Lesson processed_lesson, std::string day_of_the_we
             html::input(ctx, new_hour);
             html::p(ctx, "Minute");
             html::input(ctx, new_minute);
-            html::button(ctx, "Edit lesson", edit_lesson << new_key.read() << new_name.read() << new_hour.read() << new_minute.read());
+            html::button(ctx, "Edit lesson", edit_lesson << new_key.read() << new_name.read() << new_hour.read() << new_minute.read()
+                (
+                    edit_lesson << alia::mask(new_key.read(), new_key > 0) 
+                                << alia::mask(new_name.read(), !new_name.read().empty()) 
+                                << alia::mask(new_hour.read(), new_hour > 0 && new_hour <= 24) 
+                                << alia::mask(new_minute.read(), new_minute > 0 && new_minute < 60),
+                    new_key <<= 0,
+                    new_name <<= "",
+                    new_hour <<= 0,
+                    new_minute <<= 0,
+                )
+            );
         });
         }
         alia_end
@@ -110,7 +121,7 @@ app_ui(html::context ctx)
     document_title(ctx, "Timetable");
 
     placeholder_root(ctx, "app", [&] {
-        alia::html::bootstrap::dropdown_button(ctx, "btn-primary", "Dzień Tygodnia", [&](auto& menu) {
+        alia::html::bootstrap::dropdown_button(ctx, "btn-primary", "Weekday", [&](auto& menu) {
             menu.heading("Options");
             menu.option("Poniedziałek", active_weekday <<= "Poniedziałek");
             menu.option("Wtorek", active_weekday <<= "Wtorek");
@@ -124,7 +135,7 @@ app_ui(html::context ctx)
         });
 
         html::p(ctx, "Weekday:");
-        alia::html::bootstrap::dropdown_button(ctx, "btn-primary", "Dzień Tygodnia", [&](auto& menu) {
+        alia::html::bootstrap::dropdown_button(ctx, "btn-primary", "Weekday", [&](auto& menu) {
             menu.heading("Options");
             menu.option("Poniedziałek", new_weekday <<= "Poniedziałek");
             menu.option("Wtorek", new_weekday <<= "Wtorek");
@@ -140,7 +151,20 @@ app_ui(html::context ctx)
         html::input(ctx, new_hour);
         html::p(ctx, "Minute");
         html::input(ctx, new_minute);
-        html::button(ctx, "Add lesson", add_lesson << new_key.read() << new_name.read() << new_hour.read() << new_minute.read() << new_weekday.read());
+        html::button(ctx, "Add lesson", 
+            (
+                add_lesson << alia::mask(new_key.read(), new_key > 0) 
+                            << alia::mask(new_name.read(), !new_name.read().empty()) 
+                            << alia::mask(new_hour.read(), new_hour > 0 && new_hour <= 24) 
+                            << alia::mask(new_minute.read(), new_minute > 0 && new_minute < 60) 
+                            << new_weekday.read(),
+                new_key <<= 0,
+                new_name <<= "",
+                new_hour <<= 0,
+                new_minute <<= 0,
+                new_weekday <<= "Poniedziałek"
+            )
+        );
     });
 }
 
